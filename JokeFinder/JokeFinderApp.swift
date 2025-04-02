e88991bc14737e20697524b798defde3c2f0f8df
Hannu Packalen <hpackalen@lcs.on.ca>
2025-04-02 12:05:02 -0400

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Hannu Packalen on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
